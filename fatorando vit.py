f8c82d64601c5640f5560ea6a571ed930bd831fa
vitorzz01 <fcbprime47@gmail.com>
2025-09-02 08:40:53 -0300

    Add files via upload

---
+++ fatorando vit.py
@@ -0,0 +1,5 @@
+numero=int(input("digite o numero pra fatorar"))
+fat=1
+for i in range(1,numero+1):
+    fat*=1
+    print("o resultado do numero fatorado é",fat)
---
+++ thanos = 1 ,50.py
@@ -0,0 +1,18 @@
+thanos = 1.50
+homearanha = 1.10
+for i in range (1000):
+    if homearanha>thanos:
+        break
+    homearanha+=0.03
+    thanos+=0.02
+    print(f"vai demorar",i,"anos para que o homem aranha seja maior que o thanos!")
+
+
+
+
+
+
+
+
+
+
